4c3ca552caf2f08b08e99093992dbf0a65fd7f29
tista500 <tista.gma500@gmail.com>
2016-07-24 05:58:16 +0900

    Parse-sass.sh: Simplify and speed up

--- gtk/sass/parse-sass.sh
+++ gtk/sass/parse-sass.sh
@@ -10,20 +10,11 @@
 # (at your option) any later version.
 #
 
-# Gtk+ 3.18.x
+# Gtk+ 3.18.x, Gtk+ 3.20.x, Gtk+ 3.22.x
 bundle exec sass --update --sourcemap=none --watch \
-	3.18/gtk.scss:../gtk-3.18/gtk.css
-bundle exec sass --update --sourcemap=none --watch \
-	3.18/gtk-dark.scss:../gtk-3.18/gtk-dark.css
-
-# Gtk+ 3.20.x
-bundle exec sass --update --sourcemap=none --watch \
-	3.20/gtk.scss:../gtk-3.20/gtk.css
-bundle exec sass --update --sourcemap=none --watch \
-	3.20/gtk-dark.scss:../gtk-3.20/gtk-dark.css
-
-# Gtk+ 3.22.x
-bundle exec sass --update --sourcemap=none --watch \
-	3.22/gtk.scss:../gtk-3.22/gtk.css
-bundle exec sass --update --sourcemap=none --watch \
+	3.18/gtk.scss:../gtk-3.18/gtk.css \
+	3.18/gtk-dark.scss:../gtk-3.18/gtk-dark.css \
+	3.20/gtk.scss:../gtk-3.20/gtk.css \
+	3.20/gtk-dark.scss:../gtk-3.20/gtk-dark.css \
+	3.22/gtk.scss:../gtk-3.22/gtk.css \
 	3.22/gtk-dark.scss:../gtk-3.22/gtk-dark.css
--- shell/sass/parse-sass.sh
+++ shell/sass/parse-sass.sh
@@ -12,20 +12,13 @@
 
 # Gnome-Shell
 bundle exec sass --update --sourcemap=none --watch \
-	gnome-shell/gnome-shell.scss:../gnome-shell/gnome-shell.css
-bundle exec sass --update --sourcemap=none --watch \
-	gnome-shell/gnome-shell-dark.scss:../gnome-shell-nokto/gnome-shell.css
-
-# Gnome-Shell-Extensions
-bundle exec sass --update --sourcemap=none --watch \
-	gnome-shell/stylesheet.scss:../gnome-shell/extensions/window-list/stylesheet.css
-bundle exec sass --update --sourcemap=none --watch \
-	gnome-shell/classic.scss:../gnome-shell/extensions/window-list/classic.css
-bundle exec sass --update --sourcemap=none --watch \
+	gnome-shell/gnome-shell.scss:../gnome-shell/gnome-shell.css \
+	gnome-shell/gnome-shell-dark.scss:../gnome-shell-nokto/gnome-shell.css \
+	gnome-shell/stylesheet.scss:../gnome-shell/extensions/window-list/stylesheet.css \
+	gnome-shell/classic.scss:../gnome-shell/extensions/window-list/classic.css \
 	gnome-shell/workspaces-to-dock.scss:../gnome-shell/extensions/workspaces-to-dock/workspaces-to-dock.css
 
 # Cinnamon
 bundle exec sass --update --sourcemap=none --watch \
-	cinnamon/cinnamon.scss:../cinnamon/cinnamon.css
-bundle exec sass --update --sourcemap=none --watch \
+	cinnamon/cinnamon.scss:../cinnamon/cinnamon.css \
 	cinnamon/cinnamon-dark.scss:../cinnamon-nokto/cinnamon.css
