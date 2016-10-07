8039718c26ea600358a54c69210b7281ef312b4e
tista500 <tista.gma500@gmail.com>
2016-10-07 20:43:12 +0900

    Openbox-3: Add Openbox window-manager support
    Openbox is the lightest and stable window-manager, but I've experienced
    some limitations of theming capability.
    
      * Rounded window-frame could not be accepted.
      * Circular buttons could not be accepted.
      * Drop-shadows are rendered by 3rd-party, stand-alone compositor
        outside of Gtk+ entegration.
      * Maxd/Tiled specific theming could not be applied.
      * Resizable region depends on actual left/right/bottom frames,
        but this time I only added top decoration.
    
    So this commit is just for "not-so-bad" quality though, anyway I may
    have to refactor close button appearance after I got much time to do it.

---
+++ wm/asset/assets-openbox-scripts/clone-assets-openbox.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+#
+# This file is part of adapta-gtk-theme
+#
+# Copyright (C) 2016 Tista <tista.gma500@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+
+SRC_DIR="../assets-openbox"
+ASSETS_DIR="../../openbox-3"
+INDEX="assets-openbox.txt"
+
+# Clone stock XBM files
+for i in $(<$INDEX)
+do
+    if [ -f $ASSETS_DIR/$i.xbm ] && \
+        [ $SRC_DIR/$i.xbm -ot $ASSETS_DIR/$i.xbm ]; then
+        echo $ASSETS_DIR/$i.xbm exists.
+    elif [ -f $ASSETS_DIR/$i.xbm ] && \
+        [ $SRC_DIR/$i.xbm -nt $ASSETS_DIR/$i.xbm ]; then
+        echo Re-cloning $ASSETS_DIR/$i.xbm
+        cp $SRC_DIR/$i.xbm $ASSETS_DIR
+    else
+        echo Cloning $ASSETS_DIR/$i.xbm
+        cp $SRC_DIR/$i.xbm $ASSETS_DIR
+    fi
+done
+
+exit 0
