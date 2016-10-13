7fea449d8e788d08b3fd41d9607ff945e619fad5
tista500 <tista.gma500@gmail.com>
2016-10-13 22:50:51 +0900

    Parse-sass.sh: [Gtk] Rewrite script
    * Check Gtk+ version and generate only for current one by default.
    * Add option '-a' for watching all versioned SCSSs.

--- gtk/sass/parse-sass.sh
+++ gtk/sass/parse-sass.sh
@@ -10,41 +10,97 @@
 # (at your option) any later version.
 #
 
-# Gtk+ 3.18.x, Gtk+ 3.20.x, Gtk+ 3.22.x, Gtk+ 3.24.x
-PARALLEL="`command -v parallel`"
-THREADS="$(($(getconf _NPROCESSORS_ONLN)+1))"
+###############
+# Print usage #
+###############
 
-if [ -f "$PARALLEL" ]; then
-	$PARALLEL --no-notice --line-buffer -j$THREADS \
-        bundle exec sass --update --sourcemap=none --watch --style=compact ::: \
-		3.18/gtk.scss:../gtk-3.18/gtk.css \
-		3.18/gtk-dark.scss:../gtk-3.18/gtk-dark.css \
-		3.20/gtk.scss:../gtk-3.20/gtk.css \
-		3.20/gtk-dark.scss:../gtk-3.20/gtk-dark.css \
-		3.20/gtk-eta.scss:../gtk-3.20-eta/gtk.css \
-		3.20/gtk-dark-eta.scss:../gtk-3.20-eta/gtk-dark.css \
-		3.22/gtk.scss:../gtk-3.22/gtk.css \
-		3.22/gtk-dark.scss:../gtk-3.22/gtk-dark.css \
-		3.22/gtk-eta.scss:../gtk-3.22-eta/gtk.css \
-		3.22/gtk-dark-eta.scss:../gtk-3.22-eta/gtk-dark.css \
-		3.24/gtk.scss:../gtk-3.24/gtk.css \
-		3.24/gtk-dark.scss:../gtk-3.24/gtk-dark.css \
-		3.24/gtk-eta.scss:../gtk-3.24-eta/gtk.css \
-		3.24/gtk-dark-eta.scss:../gtk-3.24-eta/gtk-dark.css
-else
-        bundle exec sass --update --sourcemap=none --watch --style=compact \
-		3.18/gtk.scss:../gtk-3.18/gtk.css \
-		3.18/gtk-dark.scss:../gtk-3.18/gtk-dark.css \
-		3.20/gtk.scss:../gtk-3.20/gtk.css \
-		3.20/gtk-dark.scss:../gtk-3.20/gtk-dark.css \
-		3.20/gtk-eta.scss:../gtk-3.20-eta/gtk.css \
-		3.20/gtk-dark-eta.scss:../gtk-3.20-eta/gtk-dark.css \
-		3.22/gtk.scss:../gtk-3.22/gtk.css \
-		3.22/gtk-dark.scss:../gtk-3.22/gtk-dark.css \
-		3.22/gtk-eta.scss:../gtk-3.22-eta/gtk.css \
-		3.22/gtk-dark-eta.scss:../gtk-3.22-eta/gtk-dark.css \
-		3.24/gtk.scss:../gtk-3.24/gtk.css \
-		3.24/gtk-dark.scss:../gtk-3.24/gtk-dark.css \
-		3.24/gtk-eta.scss:../gtk-3.24-eta/gtk.css \
-		3.24/gtk-dark-eta.scss:../gtk-3.24-eta/gtk-dark.css
+# Usage: ./parse-sass.sh [ -a ]
+#
+# Optional arguments:
+#    -a        generates CSSs for all of Gtk+ version
+
+usage() {
+    sed -rn '/^# Usage/,${/^#/!q;s/^# ?//;p}' "$0"
+}
+
+if [ "$1" = '-h' ] || [ "$1" = '--help' ] || [ "$1" = 'help' ]; then
+    usage
+    exit 1
 fi
+
+##########################
+# Check Gtk+-3.0 version #
+##########################
+
+# evenize minor version number of gtk+-3.0
+major="`pkg-config --modversion gtk+-3.0 | cut -d. -f1`"
+minor="`pkg-config --modversion gtk+-3.0 | cut -d. -f2`"
+if [ $(expr "$minor" % 2) -ne 0 ]; then
+  css_minor="$(expr $minor + 1)"
+else
+  css_minor="$minor"
+fi
+
+#################
+# Generate CSSs #
+#################
+
+PARALLEL="`command -v parallel`"
+THREADS="$(($(getconf _NPROCESSORS_ONLN) + 1))"
+
+case "$1" in
+    -a)
+        if [ -f "$PARALLEL" ]; then
+            $PARALLEL --no-notice --line-buffer -j$THREADS \
+            bundle exec sass --update --sourcemap=none --watch --style=compact ::: \
+		3.18/gtk.scss:../gtk-3.18/gtk.css \
+		3.18/gtk-dark.scss:../gtk-3.18/gtk-dark.css \
+		3.20/gtk.scss:../gtk-3.20/gtk.css \
+		3.20/gtk-dark.scss:../gtk-3.20/gtk-dark.css \
+		3.20/gtk-eta.scss:../gtk-3.20-eta/gtk.css \
+		3.20/gtk-dark-eta.scss:../gtk-3.20-eta/gtk-dark.css \
+		3.22/gtk.scss:../gtk-3.22/gtk.css \
+		3.22/gtk-dark.scss:../gtk-3.22/gtk-dark.css \
+		3.22/gtk-eta.scss:../gtk-3.22-eta/gtk.css \
+		3.22/gtk-dark-eta.scss:../gtk-3.22-eta/gtk-dark.css \
+		3.24/gtk.scss:../gtk-3.24/gtk.css \
+		3.24/gtk-dark.scss:../gtk-3.24/gtk-dark.css \
+		3.24/gtk-eta.scss:../gtk-3.24-eta/gtk.css \
+		3.24/gtk-dark-eta.scss:../gtk-3.24-eta/gtk-dark.css
+        else
+            bundle exec sass --update --sourcemap=none --watch --style=compact \
+		3.18/gtk.scss:../gtk-3.18/gtk.css \
+		3.18/gtk-dark.scss:../gtk-3.18/gtk-dark.css \
+		3.20/gtk.scss:../gtk-3.20/gtk.css \
+		3.20/gtk-dark.scss:../gtk-3.20/gtk-dark.css \
+		3.20/gtk-eta.scss:../gtk-3.20-eta/gtk.css \
+		3.20/gtk-dark-eta.scss:../gtk-3.20-eta/gtk-dark.css \
+		3.22/gtk.scss:../gtk-3.22/gtk.css \
+		3.22/gtk-dark.scss:../gtk-3.22/gtk-dark.css \
+		3.22/gtk-eta.scss:../gtk-3.22-eta/gtk.css \
+		3.22/gtk-dark-eta.scss:../gtk-3.22-eta/gtk-dark.css \
+		3.24/gtk.scss:../gtk-3.24/gtk.css \
+		3.24/gtk-dark.scss:../gtk-3.24/gtk-dark.css \
+		3.24/gtk-eta.scss:../gtk-3.24-eta/gtk.css \
+		3.24/gtk-dark-eta.scss:../gtk-3.24-eta/gtk-dark.css
+        fi
+        ;;
+    *)
+        if [ -f "$PARALLEL" ]; then
+            $PARALLEL --no-notice --line-buffer -j$THREADS \
+            bundle exec sass --update --sourcemap=none --watch --style=compact ::: \
+		"$major"."$css_minor"/gtk.scss:../gtk-"$major"."$css_minor"/gtk.css \
+		"$major"."$css_minor"/gtk-dark.scss:../gtk-"$major"."$css_minor"/gtk-dark.css \
+		"$major"."$css_minor"/gtk-eta.scss:../gtk-"$major"."$css_minor"-eta/gtk.css \
+		"$major"."$css_minor"/gtk-dark-eta.scss:../gtk-"$major"."$css_minor"-eta/gtk-dark.css
+        else
+            bundle exec sass --update --sourcemap=none --watch --style=compact \
+		"$major"."$css_minor"/gtk.scss:../gtk-"$major"."$css_minor"/gtk.css \
+		"$major"."$css_minor"/gtk-dark.scss:../gtk-"$major"."$css_minor"/gtk-dark.css \
+		"$major"."$css_minor"/gtk-eta.scss:../gtk-"$major"."$css_minor"-eta/gtk.css \
+		"$major"."$css_minor"/gtk-dark-eta.scss:../gtk-"$major"."$css_minor"-eta/gtk-dark.css
+        fi
+        ;;
+esac
+
+exit 0
