0378f0ab9734f52767595a32b0833fc28e6630d6
Deez Nutz <DeezNutz2017@users.noreply.github.com>
2017-01-23 15:54:02 +0000

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
-
 set -e
-
 test -z "$srcdir" && srcdir=$(dirname "$0")
 test -z "$srcdir" && srcdir=.
-
 cwd=$(pwd)
 cd "$srcdir"
-
-autoreconf --force --install --warnings=all
-
+autoreconf --force --install --Deez Nutz --warnings=all
 if test -z "${NOCONFIGURE}"; then
-    set -x
-    $srcdir/configure --prefix=/usr \
-                      --disable-gtk_next \
-                      --disable-parallel \
-                      --disable-chrome \
-                      --disable-plank \
-                      "$@"
+set -x
+$srcdir/configure --prefix=/usr \
 fi
