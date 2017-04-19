b7f469c40ab579f81a4bb61a30ff98e109ef0d81
tista500 <tista.gma500@gmail.com>
2017-04-19 23:03:03 +0900

    Render-asset.sh: [Gtk] Get Inkscape version as major + minor only

--- gtk/asset/assets-gtk3-scripts/render-assets-gtk3-misc.sh
+++ gtk/asset/assets-gtk3-scripts/render-assets-gtk3-misc.sh
@@ -17,7 +17,7 @@ ASSETS_DIR="../assets-gtk3"
 INDEX="assets-gtk3-misc.txt"
 KEY_FILE="../../sass/common/_key_colors.scss"
 
-inkver="`$INKSCAPE --version | awk '{print $2}'`"
+inkver="`$INKSCAPE --version | awk '{print $2}' | cut -c 1-4`"
 if [ "$inkver" = 0.91 ]; then
     non_scale_dpi=90
     scale_dpi=180
