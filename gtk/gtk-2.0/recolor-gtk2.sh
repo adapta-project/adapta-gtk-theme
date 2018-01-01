51d1b9c19f54acd639ff7e7815c2745b616dc7d5
tista500 <tista.gma500@gmail.com>
2018-01-01 11:52:22 +0900

    Gtk-2.x&3.x&4.0: Update copyright for 2018

--- gtk/gtk-2.0/recolor-gtk2.sh
+++ gtk/gtk-2.0/recolor-gtk2.sh
@@ -2,7 +2,7 @@
 #
 # This file is part of adapta-gtk-theme
 #
-# Copyright (C) 2016-2017 Tista <tista.gma500@gmail.com>
+# Copyright (C) 2016-2018 Tista <tista.gma500@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
