56ff847acacbdd152c2774bfdaae30cacf3726a5
tista500 <tista.gma500@gmail.com>
2018-08-20 11:01:13 +0900

    Render-asset.sh: [Gtk] Integrate child scripts

--- gtk/asset/assets-gtk2-scripts/render-assets-gtk2.sh
+++ gtk/asset/assets-gtk2-scripts/render-assets-gtk2.sh
@@ -10,17 +10,89 @@
 # (at your option) any later version.
 #
 
-. "recolor-assets-gtk2.sh"
-. "render-assets-gtk2-arrow.sh"
-. "render-assets-gtk2-button.sh"
-. "render-assets-gtk2-checkradio.sh"
-. "render-assets-gtk2-column.sh"
-. "render-assets-gtk2-entry.sh"
-. "render-assets-gtk2-handle.sh"
-. "render-assets-gtk2-misc.sh"
-. "render-assets-gtk2-range.sh"
-. "render-assets-gtk2-scrollbar.sh"
-. "render-assets-gtk2-spin.sh"
-. "clone-assets-gtk2.sh"
+INKSCAPE="`command -v inkscape`"
+SRC_FILE="../assets-gtk2.svg"
+ASSETS_DIR="../assets-gtk2"
+INDEX=""
+KEY_FILE="../../sass/common/_key_colors.scss"
+
+inkver="`$INKSCAPE --version | awk '{print $2}' | cut -c 1-4`"
+if [ "$inkver" = 0.91 ]; then
+    non_scale_dpi=90
+else
+    non_scale_dpi=96
+fi
+
+# Renderer
+render-non-scale() {
+    ID=`echo $i | tr '/' '_'`
+    $INKSCAPE --export-id=$ID \
+              --export-dpi="$non_scale_dpi" \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+                                                        2>>../inkscape.log
+}
+
+# Generate PNG files
+case "$1" in
+    arrow)
+        INDEX=($(grep -e Arrows assets-gtk2.txt))
+        ;;
+    button)
+        INDEX=($(grep -e Buttons assets-gtk2.txt))
+        ;;
+    checkradio)
+        INDEX=($(grep -e Check-Radio assets-gtk2.txt))
+        ;;
+    column)
+        INDEX=($(grep -e Column assets-gtk2.txt))
+        ;;
+    entry)
+        INDEX=($(grep -e Entry assets-gtk2.txt))
+        ;;
+    handle)
+        INDEX=($(grep -e Handles assets-gtk2.txt))
+        ;;
+    misc)
+        INDEX=($(grep -e Lines -e Others -e ProgressBar -e Shadows -e Toolbar \
+              assets-gtk2.txt))
+        ;;
+    range)
+        INDEX=($(grep -e Range assets-gtk2.txt))
+        ;;
+    scrollbar)
+        INDEX=($(grep -e Scrollbars assets-gtk2.txt))
+        ;;
+    spin)
+        INDEX=($(grep -e Spin assets-gtk2.txt))
+        ;;
+    all)
+        INDEX="`cat assets-gtk2.txt`"
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+for i in ${INDEX[@]}
+do
+    SUB_DIR=`echo $i | cut -f1 -d '/'`
+    if [ '!' -d $ASSETS_DIR/$SUB_DIR ]; then
+        mkdir $ASSETS_DIR/$SUB_DIR; 
+    fi
+
+    if [ -f $ASSETS_DIR/$i.png ] && [ $KEY_FILE -ot $ASSETS_DIR/$i.png ]; then
+        echo $ASSETS_DIR/$i.png exists.
+    elif [ -f $ASSETS_DIR/$i.png ] && [ $KEY_FILE -nt $ASSETS_DIR/$i.png ]; then
+        echo Re-rendering $ASSETS_DIR/$i.png
+        echo $i.png >>../inkscape.log
+        rm -f $ASSETS_DIR/$i.png
+        render-non-scale
+    else
+        echo Rendering $ASSETS_DIR/$i.png
+        echo $i.png >>../inkscape.log
+        render-non-scale
+    fi
+done
 
 exit 0
--- gtk/asset/assets-gtk3-scripts/render-assets-gtk3.sh
+++ gtk/asset/assets-gtk3-scripts/render-assets-gtk3.sh
@@ -10,13 +10,101 @@
 # (at your option) any later version.
 #
 
-. "recolor-assets-gtk3.sh"
-. "render-assets-gtk3-checkbox.sh"
-. "render-assets-gtk3-misc.sh"
-. "render-assets-gtk3-radio.sh"
-. "render-assets-gtk3-slider.sh"
-. "render-assets-gtk3-toggle.sh"
-. "render-assets-gtk3-windowbutton.sh"
-. "render-assets-gtk3-windowbutton-eta.sh"
+INKSCAPE="`command -v inkscape`"
 
-exit 0
+SRC_FILE="../assets-gtk3.svg"
+ASSETS_DIR="../assets-gtk3"
+INDEX=""
+KEY_FILE="../../sass/common/_key_colors.scss"
+
+inkver="`$INKSCAPE --version | awk '{print $2}' | cut -c 1-4`"
+if [ "$inkver" = 0.91 ]; then
+    non_scale_dpi=90
+    scale_dpi=180
+else
+    non_scale_dpi=96
+    scale_dpi=192
+fi
+
+#Renderer
+render-non-scale() {
+    $INKSCAPE --export-id=$i \
+              --export-dpi="$non_scale_dpi" \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+                                                        2>>../inkscape.log
+}
+
+render-scale() {
+    $INKSCAPE --export-id=$i \
+              --export-dpi="$scale_dpi" \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null \
+                                                          2>>../inkscape.log
+}
+
+# Generate PNG files
+case "$1" in
+    checkbox)
+        INDEX=($(grep -e checkbox assets-gtk3.txt))
+        ;;
+    misc)
+        INDEX=($(grep -e osd -e thumb -e needs assets-gtk3.txt))
+        ;;
+    radio)
+        INDEX=($(grep -e radio assets-gtk3.txt))
+        ;;
+    slider)
+        INDEX=($(grep -e slider assets-gtk3.txt))
+        ;;
+    toggle)
+        INDEX=($(grep -e toggle assets-gtk3.txt))
+        ;;
+    window-close)
+        INDEX=($(grep -e 'window-close' assets-gtk3.txt))
+        ;;
+    window-minimize)
+        INDEX=($(grep -e 'window-minimize' assets-gtk3.txt))
+        ;;
+    window-maximize)
+        INDEX=($(grep -e 'window-maximize' assets-gtk3.txt))
+        ;;
+    window-unmaximize)
+        INDEX=($(grep -e 'window-unmaximize' assets-gtk3.txt))
+        ;;
+    all)
+        INDEX="`cat assets-gtk3.txt`"
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+for i in ${INDEX[@]}
+do 
+    if [ -f $ASSETS_DIR/$i.png ] && [ $KEY_FILE -ot $ASSETS_DIR/$i.png ]; then
+        echo $ASSETS_DIR/$i.png exists.
+    elif [ -f $ASSETS_DIR/$i.png ] && [ $KEY_FILE -nt $ASSETS_DIR/$i.png ]; then
+        echo Re-rendering $ASSETS_DIR/$i.png
+        echo $i.png >>../inkscape.log
+        rm -f $ASSETS_DIR/$i.png
+        render-non-scale
+    else
+        echo Rendering $ASSETS_DIR/$i.png
+        echo $i.png >>../inkscape.log
+        render-non-scale
+    fi
+
+    if [ -f $ASSETS_DIR/$i@2.png ] && [ $KEY_FILE -ot $ASSETS_DIR/$i@2.png ]; then
+        echo $ASSETS_DIR/$i@2.png exists.
+    elif [ -f $ASSETS_DIR/$i@2.png ] && [ $KEY_FILE -nt $ASSETS_DIR/$i@2.png ]; then
+        echo Re-rendering $ASSETS_DIR/$i@2.png
+        echo $i@2.png >>../inkscape.log
+        rm -f $ASSETS_DIR/$i@2.png
+        render-scale
+    else
+        echo Rendering $ASSETS_DIR/$i@2.png
+        echo $i@2.png >>../inkscape.log
+        render-scale
+    fi
+done
