c13d3334e617d92c9f6e5bfaf57fc5452223d17b
terminalforlife <terminalforlife@yahoo.com>
2019-11-13 22:47:35 +0000

    Optimize by utilizing `bash`, and condense code
    **The following is taken from my commit message on the below link:**
    https://github.com/terminalforlife/adapta-midnight/commit/5e996bd24ce203ff87995bbf62e9f71a7b3f5adf
    
    Since `bash` is being used, it seems logical to _actually_ use as many
    such features as possible and appropriate, of which there are plenty.
    
    A fair bit was done here, so I'll try to be concise:
    
      * Three functions were created for condensed code and easier editing.
    
      * The fetching and/or assignment of many variables were optimized.
    
      * Additional variables and strings were correctly quoted.
    
      * An unprotected string causing undesired results was corrected.
    
    Overall, I believe this script is now a _lot_ easier to read and edit.

--- shell/asset/clone-assets-cinnamon.sh
+++ shell/asset/clone-assets-cinnamon.sh
@@ -16,96 +16,90 @@ ASSETS_DARK_DIR="../cinnamon-nokto/assets"
 INDEX="assets-cinnamon.txt"
 KEY_FILE="../../gtk/sass/common/_key_colors.scss"
 
+Recolor(){
+    if [ "$1" != "$2" ]; then
+        sed -i "s/$1/$2/gi" "$SRC_DIR/$3"
+        echo "$3 is re-colored with $2."
+    fi
+}
+
+Cloning(){ echo "Cloning $2"; cp "$1" "$2"; }
+Exists(){ echo "$1 exists."; }
+
 # Default colours
-selection1="`grep 'Cyan500' ../../gtk/sass/common/_colors.scss | \
-                   cut -d' ' -f3`"
-accent1="`grep 'Teal300' ../../gtk/sass/common/_colors.scss | \
-                cut -d' ' -f3`"
-destruction1="`grep 'RedA200' ../../gtk/sass/common/_colors.scss | \
-                     cut -d' ' -f3`"
+while read -a LINE; do
+    case "${LINE[*]}" in
+        *Cyan500*) selection1="${LINE[2]}" ;;
+        *Teal300*) accent1="${LINE[2]}" ;;
+        *RedA200*) destruction1="${LINE[2]}" ;;
+    esac
+done < ../../gtk/sass/common/_colors.scss
 
 # Check and re-color SVG files
-if [ -e $KEY_FILE ]; then
-    selection2="`grep 'key_selection' $KEY_FILE | \
-                 cut -d' ' -f2 | cut -d';' -f1`"
-    accent2="`grep 'key_accent' $KEY_FILE | \
-              cut -d' ' -f2 | cut -d';' -f1`"
-    destruction2="`grep 'key_destruction' $KEY_FILE | \
-                   cut -d' ' -f2 | cut -d';' -f1`"
+if [ -f "$KEY_FILE" ]; then
+    while read -a LINE; do
+        case "${LINE[*]}" in
+              *key_selection*) selection2=${LINE[1]%;} ;;
+                 *key_accent*) accent2=${LINE[1]%;} ;;
+            *key_destruction*) destruction2=${LINE[1]%;} ;;
+        esac
+    done < "$KEY_FILE"
 
-IFS=$'
-'
-for i in $(<$INDEX)
-do
-    s="`echo $i | cut -d' ' -f1`.svg" # source
-    r="`echo $i | cut -d' ' -f2`" # recolor flag
+    Clone
 
-    if [ "$r" = "r1" ]; then
-        cp -f $SRC_DIR/$s.in $SRC_DIR/$s
+    IFS=$'\n'
+    for i in $(<"$INDEX"); do
+        s="${i%% *}" # source
+        r="${i#* }" # recolor flag
 
-        if [ $selection1 != $selection2 ]; then
-            sed -i "s/$selection1/$selection2/gi" $SRC_DIR/$s
-            echo $s is re-colored with $selection2.
+        if [ "$r" = "r1" ]; then
+            cp -f "$SRC_DIR/$s.in" "$SRC_DIR/$s"
+            Recolor "$selection1" "$selection2" "$s"
+        elif [ "$r" = "r2" ]; then
+            cp -f "$SRC_DIR/$s.in" "$SRC_DIR/$s"
+            Recolor "$accent1" "$accent2" "$s"
+        elif [ "$r" = "r3" ]; then
+            cp -f "$SRC_DIR/$s.in" "$SRC_DIR/$s"
+            Recolor "$destruction1" "$destruction2" "$s"
         fi
-    elif [ "$r" = "r2" ]; then
-        cp -f $SRC_DIR/$s.in $SRC_DIR/$s
-
-        if [ $accent1 != $accent2 ]; then
-            sed -i "s/$accent1/$accent2/gi" $SRC_DIR/$s
-            echo $s is re-colored with $accent2.
-        fi
-    elif [ "$r" = "r3" ]; then
-        cp -f $SRC_DIR/$s.in $SRC_DIR/$s
-
-        if [ $destruction1 != $destruction2 ]; then
-            sed -i "s/$destruction1/$destruction2/gi" $SRC_DIR/$s
-            echo $s is re-colored with $destruction2.
-        fi
-    fi
-done
-unset IFS
-
+    done
+    unset IFS
 else
-    echo _key_colors.scss was not found. Stopped...
+    echo "_key_colors.scss was not found. Stopped..." >&2
     exit 1
 fi
 
 # Clone stock SVG files
-IFS=$'
-'
-for i in $(<$INDEX)
-do
-    s="`echo $i | cut -d' ' -f1`.svg"                 # source
-    f="`echo $i | cut -d' ' -f1 | cut -d'/' -f3`.svg" # file name
-    v="`echo $i | cut -c1`"                           # variant type
-    d="`echo $i | cut -d'/' -f2`"                     # target directory
+IFS=$'\n'
 
-    if [ $v = "c" ]; then # 'commmon'
-        if [ -f $ASSETS_DIR/$d/$f ] && \
-            [ $SRC_DIR/$s -ot $ASSETS_DIR/$d/$f ]; then
-            echo $ASSETS_DIR/$d/$f exists.
-            echo $ASSETS_DARK_DIR/$d/$f exists.
+for i in $(<"$INDEX"); do
+    s="${i%% *}.svg"                        # source
+    f="$(cut -d'/' -f3 <<< "${i%% *}"`.svg  # file name
+    v="${i:0:1}"                            # variant type
+    d="${i#?*/}"                            # target directory
+
+    if [ "$v" = "c" ]; then # 'commmon'
+        if [ -f "$ASSETS_DIR/$d/$f" ] && \
+            [ "$SRC_DIR/$s" -ot "$ASSETS_DIR/$d/$f" ]; then
+            Exists "$ASSETS_DIR/$d/$f"
+            Exists "$ASSETS_DARK_DIR/$d/$f"
         else
-            echo Cloning $ASSETS_DIR/$d/$f
-            cp $SRC_DIR/$s $ASSETS_DIR/$d/$f
-            echo Cloning $ASSETS_DARK_DIR/$d/$f
-            cp $SRC_DIR/$s $ASSETS_DARK_DIR/$d/$f
+            Cloning "$SRC_DIR/$s" "$ASSETS_DIR/$d/$f"
+            Cloning "$SRC_DIR/$s" "$ASSETS_DARK_DIR/$d/$f"
         fi
-    elif [ $v = "l" ]; then # 'light'
-        if [ -f $ASSETS_DIR/$d/$f ] && \
-            [ $SRC_DIR/$s -ot $ASSETS_DIR/$d/$f ]; then
-            echo $ASSETS_DIR/$d/$f exists.
+    elif [ "$v" = "l" ]; then # 'light'
+        if [ -f "$ASSETS_DIR/$d/$f" ]\
+        && [ "$SRC_DIR/$s" -ot "$ASSETS_DIR/$d/$f" ]; then
+            Exists "$ASSETS_DIR/$d/$f"
         else
-            echo Cloning $ASSETS_DIR/$d/$f
-            cp $SRC_DIR/$s $ASSETS_DIR/$d/$f
+            Cloning "$SRC_DIR/$s" "$ASSETS_DIR/$d/$f"
         fi
     else # 'dark'
-        if [ -f $ASSETS_DARK_DIR/$d/$f ] && \
-            [ $SRC_DIR/$s -ot $ASSETS_DARK_DIR/$d/$f ]; then
-            echo $ASSETS_DARK_DIR/$d/$f exists.
+        if [ -f "$ASSETS_DARK_DIR/$d/$f" ]\
+        && [ "$SRC_DIR/$s" -ot "$ASSETS_DARK_DIR/$d/$f" ]; then
+            Exists "$ASSETS_DARK_DIR/$d/$f"
         else
-            echo Cloning $ASSETS_DARK_DIR/$d/$f
-            cp $SRC_DIR/$s $ASSETS_DARK_DIR/$d/$f
+            Cloning "$SRC_DIR/$s" "$ASSETS_DARK_DIR/$d/$f"
         fi
     fi
 done
